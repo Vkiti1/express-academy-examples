5e6c7a1c0d335d0c4bca8843c7793c79de54cc68
Viktor Škifić <viktor.skific@gmail.com>
2021-08-16 13:06:11 +0200

    error handling and jwt assignments

--- src/index.js
+++ src/index.js
@@ -1,30 +1,121 @@
 // Load environment variable from .env file if not supplied
 const path = require('path');
 require('dotenv').config({
-  path: (typeof process.env.DOTENV_PATH !== 'undefined')
-    ? path.resolve(process.cwd(), process.env.DOTENV_PATH)
-    : path.resolve(process.cwd(), '.env'),
+	path:
+		typeof process.env.DOTENV_PATH !== 'undefined'
+			? path.resolve(process.cwd(), process.env.DOTENV_PATH)
+			: path.resolve(process.cwd(), '.env'),
 });
 
 // --- Start ---
-const express = require('express')
+const pretty = require('../src/utils/pretty');
+const _ = require('lodash');
+const express = require('express');
 const morgan = require('morgan');
+const jwt = require('jsonwebtoken');
+const jwtExpress = require('express-jwt');
+const loadPrivateKey = require('./utils/loadPrivateKey');
+const {
+	customCORSAndAuthErrorMiddleware,
+} = require('./utils/customMiddleware');
+const { ERROR_PAGE_NOT_FOUND, errorDescriptor } = require('./errors/index');
 
-const app = express()
+const { certPrivate, certPublic } = loadPrivateKey();
+
+const app = express();
 
 app.use(morgan('combined'));
 
 app.get('/', (req, res) => {
-  res.send('Home Page')
-})
+	res.send('Home Page');
+});
 
 app.get('/users', (req, res) => {
-  res.send('Users Page!')
-})
+	res.send('Users Page!');
+});
 
+// TODO
+app.get(
+	'/my-profile',
+	jwtExpress({
+		secret: certPublic,
+		algorithms: ['RS256'],
+		requestProperty: 'auth',
+	}),
+	customCORSAndAuthErrorMiddleware,
+	(req, res, next) => {
+		if (!req.auth) {
+			return res.sendStatus(403);
+		}
+		next();
+	},
+	(req, res) => {
+		const dummyProfile = {
+			firstName: 'Viktor',
+			lastName: 'Škifić',
+			dob: new Date(1997, 1, 27),
+		};
+
+		res.send(dummyProfile);
+	}
+);
+
+app.get('/public', (req, res) => {
+	res.type('plain/text');
+	res.send(certPublic);
+});
+
+app.get('/sign/:id', (req, res) => {
+	const result = jwt.sign({ id: req.params.id }, certPrivate, {
+		algorithm: 'RS256',
+	});
+	res.send(result);
+});
+
+// 404
+app.use((req, res, next) => {
+	next(new Error(errorDescriptor(ERROR_PAGE_NOT_FOUND)));
+});
+
+app.use((error, req, res, next) => {
+	// Try to parse the error, if unable log it and set it to default one.
+	let errorOut = {};
+	let parsed = {};
+	try {
+		parsed = JSON.parse(error.message);
+		errorOut = {
+			...parsed,
+		};
+	} catch (e) {
+		console.log(
+			'[API - ERROR] Unknown error, unable to parse the error key, code and message:'
+				.bgYellow.black
+		);
+		console.error(error);
+		errorOut.code = 500;
+		errorOut.key = 'INTERNAL_SERVER_ERROR';
+		errorOut.message = error.message;
+	}
+
+	// Audit the API errors
+	// Get IP
+	const ip =
+		_.get(req, 'headers.x-forwarded-for') ||
+		_.get(req, 'connection.remoteAddress');
+
+	// Send the response to the client
+	res.setHeader('Content-Type', 'application/json');
+	res.status(errorOut.code).send(
+		pretty({
+			error: 1,
+			errors: [{ ...errorOut }],
+			data: null,
+		})
+	);
+});
 
 const port = process.env.PORT;
 
 app.listen(port, () => {
-  console.log(`🚀  Server ready at ${port}`);
-})
+	console.log(`🚀  Server ready at ${port}`);
+});
--- src/utils/customMiddleware.js
+++ src/utils/customMiddleware.js
@@ -1,10 +1,10 @@
 const _ = require('lodash');
-const { 
-  errorDescriptor,
-  ERROR_INVALID_AUTHENTICATION_TOKEN,
-  ERROR_INVALID_AUTHENTICATION_TOKEN_BAD_FORMAT,
-  ERROR_INVALID_CORS_POLICY,
-  ERROR_INTERNAL_SERVER_ERROR,
+const {
+	errorDescriptor,
+	ERROR_INVALID_AUTHENTICATION_TOKEN,
+	ERROR_INVALID_AUTHENTICATION_TOKEN_BAD_FORMAT,
+	ERROR_INVALID_CORS_POLICY,
+	ERROR_INTERNAL_SERVER_ERROR,
 } = require('./../errors');
 const jwtExpressFunction = require('express-jwt');
 
@@ -15,38 +15,52 @@ const jwtExpressFunction = require('express-jwt');
 // });
 
 const customCORSAndAuthErrorMiddleware = (err, req, res, next) => {
-  if (err.name === 'UnauthorizedError' && _.get(err, 'code') === 'credentials_required') {
-    next(new Error(errorDescriptor(ERROR_INVALID_AUTHENTICATION_TOKEN)));
-    return;
-  }
-  if (err.name === 'UnauthorizedError' && _.get(err, 'code') === 'credentials_bad_format') {
-    next(new Error(errorDescriptor(ERROR_INVALID_AUTHENTICATION_TOKEN_BAD_FORMAT)));
-    return;
-  }
-  if (err.name === 'UnauthorizedError' && _.get(err, 'code') === 'invalid_token') { // If there is invalid signature
-    next(new Error(errorDescriptor(ERROR_INVALID_AUTHENTICATION_TOKEN)));
-    return;
-  }
-  if (err.name === 'UnauthorizedError') {
-    console.log(Array(80 + 1).join('-'));
-    console.log('There is UnauthorizedError from jwtExpress, that has no custom error message.');
-    console.log(JSON.stringify(err, null, 2)); // Log the error message
-    next(new Error(errorDescriptor(ERROR_INVALID_AUTHENTICATION_TOKEN)));
-    return;
-  }
-  if (err.name === 'NotAllowedByCORSPolicy') {
-    next(new Error(errorDescriptor(ERROR_INVALID_CORS_POLICY)));
-    return;
-  }
-  if (err) {
-    console.log(JSON.stringify(err, null, 2));
-    next(new Error(errorDescriptor(ERROR_INTERNAL_SERVER_ERROR)));
-    return;
-  }
-  next();
+	if (
+		err.name === 'UnauthorizedError' &&
+		_.get(err, 'code') === 'credentials_required'
+	) {
+		next(new Error(errorDescriptor(ERROR_INVALID_AUTHENTICATION_TOKEN)));
+		return;
+	}
+	if (
+		err.name === 'UnauthorizedError' &&
+		_.get(err, 'code') === 'credentials_bad_format'
+	) {
+		next(
+			new Error(errorDescriptor(ERROR_INVALID_AUTHENTICATION_TOKEN_BAD_FORMAT))
+		);
+		return;
+	}
+	if (
+		err.name === 'UnauthorizedError' &&
+		_.get(err, 'code') === 'invalid_token'
+	) {
+		// If there is invalid signature
+		next(new Error(errorDescriptor(ERROR_INVALID_AUTHENTICATION_TOKEN)));
+		return;
+	}
+	if (err.name === 'UnauthorizedError') {
+		console.log(Array(80 + 1).join('-'));
+		console.log(
+			'There is UnauthorizedError from jwtExpress, that has no custom error message.'
+		);
+		console.log(JSON.stringify(err, null, 2)); // Log the error message
+		next(new Error(errorDescriptor(ERROR_INVALID_AUTHENTICATION_TOKEN)));
+		return;
+	}
+	if (err.name === 'NotAllowedByCORSPolicy') {
+		next(new Error(errorDescriptor(ERROR_INVALID_CORS_POLICY)));
+		return;
+	}
+	if (err.name === 'InternalServerError') {
+		console.log(JSON.stringify(err, null, 2));
+		next(new Error(errorDescriptor(ERROR_INTERNAL_SERVER_ERROR)));
+		return;
+	}
+	next();
 };
 
 module.exports = {
-  // jwtExpress,
-  customCORSAndAuthErrorMiddleware,
+	// jwtExpress,
+	customCORSAndAuthErrorMiddleware,
 };
